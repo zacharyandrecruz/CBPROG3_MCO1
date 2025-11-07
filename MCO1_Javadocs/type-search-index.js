1e449637fdd0a85f2fbbef5582f9bbebc9107ded
AgentAcee198 <alainecarlo2@gmail.com>
2025-11-07 15:50:09 +0800

    Ver 0.6
    Added Generated Javadocs.

---
+++ MCO1_Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bank"},{"p":"<Unnamed>","l":"Budget"},{"p":"<Unnamed>","l":"DateTime"},{"p":"<Unnamed>","l":"Expense"},{"p":"<Unnamed>","l":"ExpenseTracker"},{"p":"<Unnamed>","l":"User"}];updateSearchResults();
